4c4119e56ba045bd252453e81ee10d9db8c36353
arifs16 <arifsetyadi16@gmail.com>
2023-04-06 10:22:14 +0700

    perubahan dari prosedural ke oop

--- recentearthquake/__init__.py
+++ recentearthquake/__init__.py
@@ -1,74 +1,84 @@
 import requests
 import bs4
 
-description = 'To get the recent earthquake in Indonesia from BMKG.go.id'
-def ekstraksi_data():
+class RecentEarthquake:
+    def __init__(self):
+        self.description = 'To get the recent earthquake in Indonesia from BMKG.go.id'
+        self.result = None
 
-    try:
-        content = requests.get('https://bmkg.go.id')
-    except Exception:
-        return None
-    if content.status_code == 200:
-        soup = bs4.BeautifulSoup(content.text, 'html.parser')
+    def ekstraksi_data(self):
 
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        tanggal = result[0]
-        waktu = result[1]
+        try:
+            content = requests.get('https://bmkg.go.id')
+        except Exception:
+            return None
+        if content.status_code == 200:
+            soup = bs4.BeautifulSoup(content.text, 'html.parser') #INSTANTIATION = INSTANTIASI = PENCIPTAAN OBJECT DARI CLASS
 
-        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
-        i = 0
-        magnitudo = None
-        kedalaman = None
-        ls = None
-        bt = None
-        lokasi = None
-        dirasakan = None
+            result = soup.find('span', {'class': 'waktu'})
+            result = result.text.split(', ')
+            tanggal = result[0]
+            waktu = result[1]
 
-        for res in result:
-            if i == 1:
-                magnitudo =  res.text
-            elif i == 2:
-                kedalaman = res.text
-            elif i == 3:
-                koordinat = res.text.split(' - ')
-                ls = koordinat[0]
-                bt = koordinat[1]
-            elif i == 4:
-                lokasi = res.text
-            elif i == 5:
-                dirasakan = res.text
-            i = i + 1
+            result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+            result = result.findChildren('li')
+            i = 0
+            magnitudo = None
+            kedalaman = None
+            ls = None
+            bt = None
+            lokasi = None
+            dirasakan = None
 
-        hasil = dict()
-        hasil['tanggal'] = tanggal
-        hasil['waktu'] = waktu
-        hasil['magnitudo'] = magnitudo
-        hasil['kedalaman'] = kedalaman
-        hasil['koordinat'] = {'ls': ls, 'bt': bt}
-        hasil['lokasi'] = lokasi
-        hasil['dirasakan'] = dirasakan
-        return hasil
-    else:
-        return None
+            for res in result:
+                if i == 1:
+                    magnitudo =  res.text
+                elif i == 2:
+                    kedalaman = res.text
+                elif i == 3:
+                    koordinat = res.text.split(' - ')
+                    ls = koordinat[0]
+                    bt = koordinat[1]
+                elif i == 4:
+                    lokasi = res.text
+                elif i == 5:
+                    dirasakan = res.text
+                i = i + 1
+
+            hasil = dict()
+            hasil['tanggal'] = tanggal
+            hasil['waktu'] = waktu
+            hasil['magnitudo'] = magnitudo
+            hasil['kedalaman'] = kedalaman
+            hasil['koordinat'] = {'ls': ls, 'bt': bt}
+            hasil['lokasi'] = lokasi
+            hasil['dirasakan'] = dirasakan
+            self.result = hasil
+        else:
+            return None
 
 
-def tampilkan_data(result):
-    if result is None:
-        print('Tidak bisa menemukan data gempa terkini')
-        return
+    def tampilkan_data(self):
+        if self.result is None:
+            print('Tidak bisa menemukan data gempa terkini')
+            return
 
-    print('The last earthquake based on BMKG')
-    print(f"Date {result['tanggal']}")
-    print(f"Time {result['waktu']}")
-    print(f"Magnitude {result['magnitudo']}")
-    print(f"Depth {result['kedalaman']}")
-    print(f"Coordinate: {result['koordinat']['ls']}, {result['koordinat']['bt']}")
-    print(f"Location {result['lokasi']}")
-    print(f"{result['dirasakan']}")
+        print('The last earthquake based on BMKG')
+        print(f"Date {self.result['tanggal']}")
+        print(f"Time {self.result['waktu']}")
+        print(f"Magnitude {self.result['magnitudo']}")
+        print(f"Depth {self.result['kedalaman']}")
+        print(f"Coordinate: {self.result['koordinat']['ls']}, {self.result['koordinat']['bt']}")
+        print(f"Location {self.result['lokasi']}")
+        print(f"{self.result['dirasakan']}")
+
+    def run(self):
+        self.ekstraksi_data()
+        self.tampilkan_data()
 
 if __name__ == '__main__':
-    print('Package description', description)
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    earthquake_in_indonesia = RecentEarthquake()
+    print('Package description', earthquake_in_indonesia.description)
+    earthquake_in_indonesia.run()
+#        earthquake_in_indonesia.ekstraksi_data()
+#       earthquake_in_indonesia.tampilkan_data()

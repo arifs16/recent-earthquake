b9c736a0cead2b8da24bd9cd00d35551d07fff68
arifs16 <arifsetyadi16@gmail.com>
2023-04-06 08:19:49 +0700

    added description

--- recentearthquake/__init__.py
+++ recentearthquake/__init__.py
@@ -1,7 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 
-
+description = 'To get the recent earthquake in Indonesia from BMKG.go.id'
 def ekstraksi_data():
 
     try:
@@ -9,7 +9,7 @@ def ekstraksi_data():
     except Exception:
         return None
     if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
+        soup = bs4.BeautifulSoup(content.text, 'html.parser')
 
         result = soup.find('span', {'class': 'waktu'})
         result = result.text.split(', ')
